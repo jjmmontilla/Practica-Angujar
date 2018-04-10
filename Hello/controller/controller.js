9b47bbcd2471e53aa09e63dc0a8abdc2cef43215
jjmmontilla <jjmmontilla@gmail.com>
2018-04-09 22:29:33 -0500

    Guardando en el local storage con un factory

--- Hello/controller/controller.js
+++ Hello/controller/controller.js
@@ -55,20 +55,88 @@ app.controller("CommentsAPI", ["$scope", "$http", function(e, http) {
 }]
 );
 
-app.controller("ToDoList", ["$scope", "$http", "localStorageService", function(e, http, storage){
-	e.todo = [];
-	e.newToDo = {};
-
-	if(storage.get('angular-todoList')){
-		e.todo = storage.get('angular-todoList');
+app.factory('ToDoService', function(localStorageService){
+	var toDoService = {};
+	toDoService.key = "angular-todoList";
+	if(localStorageService.get(toDoService.key)){
+		toDoService.activities = localStorageService.get(toDoService.key);
 	}else{
-		e.todo = [];
+		toDoService.activities =[];
 	}
 
+	toDoService.add = function(newToDo){
+		toDoService.activities.push(newToDo);
+		toDoService.updateStorage();
+	}
+
+	toDoService.updateStorage = function(){
+		localStorageService.set(toDoService.key, toDoService.activities);
+	}
+	
+	toDoService.clean = function(){
+		toDoService.activities = [];
+		toDoService.updateStorage();
+		return toDoService.getAll();
+	}
+
+	toDoService.getAll = function(){
+		return toDoService.activities;
+	}
+	toDoService.removeItem = function(item){
+		toDoService.activities = toDoService.activities.filter(function(activity){
+			return activity !== item;
+		})
+		toDoService.updateStorage();
+		return toDoService.getAll();
+	}
+
+	return toDoService;
+});
+
+app.controller("ToDoList", ["$scope", "ToDoService", function(e, ToDoService){
+	e.todo = ToDoService.getAll();
+	e.newToDo = {};
+
 	e.addActividad = function(){
-		e.todo.push(e.newToDo);
-		storage.set("angular-todoList", e.todo);
-		e.newToDo = {};
+		ToDoService.add(e.newToDo);
+		e.newToDo = {} 
+	}
+
+	e.removeItem = function(item){
+		e.todo = ToDoService.removeItem(item);
+	}
+	e.clean = function(){
+		e.todo = ToDoService.clean()
 	}
 }]
 );
+
+app.filter("removeHtml", function(){
+	return function(texto){
+		return String(texto).replace(/<[^>]+>/gm, '');
+	}
+});
+
+app.controller("FiltersController", ["$scope", "localStorageService", function(e, storage){
+	e.listHtml = [];
+	e.newHtml = {};
+
+	if(storage.get('listHtml')){
+		e.listHtml = storage.get('listHtml');
+	}else{
+		e.listHtml = [];
+	}
+
+	e.addHtml = function(){
+		e.listHtml.push(e.newHtml);
+		storage.set("listHtml", e.listHtml);
+		e.newHtml = {};
+	}
+
+	e.cleanHtml = function(){
+		storage.set("listHtml", []);
+	}
+
+}]
+);
+
